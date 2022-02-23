5ce91c284e03cb46bb2ca4ec5fb512d94fe04c45
deniskaanikov <65065113+deniskaanikov@users.noreply.github.com>
2022-02-23 18:55:47 +0300

    Update hw_from_19.02.cpp

--- hw_from_19.02.cpp
+++ hw_from_19.02.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 //функция для отражения числа
-int mirror(int start, int obr, int a) {
+int mirror(int start) {
+	int obr = 0;
+	int a = 0;
 	while (start > 0) {
 		a = start % 10;
 		start = start / 10;
@@ -12,17 +15,26 @@ int mirror(int start, int obr, int a) {
 	return obr;
 }
 
-//функиця для поиска отрицательных чисел
-int search(int number) {
-	while 
-}
+//функиця для поиска неположительных чисел
+/*
+int search(string number) {
+	int counter = 0;
+	string number = "  ";
+	while (number != "") {
+		cin >> number >> endl;
+		int n = stoi(number);
+		if (n <= 0) {
+			counter = 1;
+		}
+	}
+	return counter;
+} 
+*/
 
 int main(){
 	int x;
-	int y;
-	int z;
-	y = z = 0;
 	cin >> x;
-	cout << mirror (x, y, z) << endl;
+	cout << mirror (x) << endl;
+	//cout << search(h, c);
 	return 0;
 }
